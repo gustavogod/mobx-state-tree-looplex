9fc527475eac56e580df1d01f39ce2979c5e93ed
gustavogod <gustavodias.god@gmail.com>
2021-11-29 17:22:33 -0300

    first commit

---
+++ src/components/WishListItemView.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react"
+import { observer } from "mobx-react"
+import { clone, getSnapshot, applySnapshot } from "mobx-state-tree"
+
+import WishListItemEdit from "./WishListItemEdit"
+
+class WishListItemView extends Component {
+    constructor() {
+        super()
+        this.state = { isEditing: false }
+    }
+
+    render() {
+        const { item, readonly } = this.props
+        return this.state.isEditing ? (
+            this.renderEditable()
+        ) : (
+            <li className="item">
+                {item.image && <img src={item.image} />}
+                <h3>{item.name}</h3>
+                <span>{item.price} €</span>
+                {!readonly && (
+                    <span>
+                        <button onClick={this.onToggleEdit}>✏</button>
+                        <button onClick={item.remove}>❎</button>
+                    </span>
+                )}
+            </li>
+        )
+    }
+
+    renderEditable() {
+        return (
+            <li className="item">
+                <WishListItemEdit item={this.state.clone} />
+                <button onClick={this.onSaveEdit}>💾</button>
+                <button onClick={this.onCancelEdit}>❎</button>
+            </li>
+        )
+    }
+
+    onToggleEdit = () => {
+        this.setState({
+            isEditing: true,
+            clone: clone(this.props.item)
+        })
+    }
+
+    onCancelEdit = () => {
+        this.setState({ isEditing: false })
+    }
+
+    onSaveEdit = () => {
+        applySnapshot(this.props.item, getSnapshot(this.state.clone))
+        this.setState({
+            isEditing: false,
+            clone: null
+        })
+    }
+}
+
+export default observer(WishListItemView)
+//com o observer, sempre que algum dado relevante para a renderização do componente é alterado, então o compoenente sofre o rerender
+//assim o mobk é conectado com o react
